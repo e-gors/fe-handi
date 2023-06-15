95b406e37a680f60d9127c0f0ad813c61fad548c
efren goron <egoronweb@gmail.com>
2023-06-15 22:03:40 +0800

    changing .env.example to .env

--- build/static/js/161.0a427014.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfe_handi=self.webpackChunkfe_handi||[]).push([[161],{80161:function(n,t,c){c.r(t),c.d(t,{login:function(){return a}});var e=c(31243),a=function(n){return e.Z.post("".concat("http://localhost:8000/api","/login"),n,{headers:{Accept:"application/json"}})}}}]);
-//# sourceMappingURL=161.0a427014.chunk.js.map
