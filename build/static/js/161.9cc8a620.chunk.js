1a6f68c93f72378f2e6f4e74f83f78b38d2d7003
efren goron <egoronweb@gmail.com>
2023-06-16 11:12:57 +0800

    change http to https

--- build/static/js/161.9cc8a620.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfe_handi=self.webpackChunkfe_handi||[]).push([[161],{80161:function(n,e,t){t.r(e),t.d(e,{login:function(){return a}});var i=t(31243),a=function(n){return i.Z.post("".concat("http://api-handiworks.technopreneurship.store/api","/login"),n,{headers:{Accept:"application/json"}})}}}]);
-//# sourceMappingURL=161.9cc8a620.chunk.js.map
